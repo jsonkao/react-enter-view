4bd0e3a0af99d803892bfcd281aa022a4f40af02
Jason Kao <jasonkao85@gmail.com>
2018-07-10 10:29:48 -0400

    potential security vulnerabilities

--- src/js/versionInfo.js
+++ src/js/versionInfo.js
@@ -3,5 +3,5 @@
  *           without modifying formatting for automation scripts
  * @type {{VERSION: string}}
  */
-let VersionInfo = { VERSION : '0.0.23' };
+let VersionInfo = { VERSION : '0.0.26' };
 export default VersionInfo;
